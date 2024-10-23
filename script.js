96b7f6b4f6d65fb87ab473326df2c9ad650c45b5
Aaron <shihaaron40@gmail.com>
2024-10-23 19:48:19 +0000

    Copy over files.

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+let img;
 
+function preload() {
+  img = loadImage("images.png");
+}
+
+function displayTitle() {
+  textSize(15);
+  fill(0);
+  text("Welcome to Memory Lane", 10, 20);
+}
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  createCanvas(200, 300);
+  let button = createButton('Start Game');
+  button.position(70, 270);
+  button.mousePressed(startGame);
 }
 
 function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  background(220);
+  image(img, 20, 70, 150, 150);
+  displayTitle();
+}
+
+function startGame() {
+  window.open("");
 }
